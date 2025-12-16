473b1eade66545f82fa7efa20982aa63542176e8
Steftrix <zaborilastefan5@gmail.com>
2025-12-16 13:39:50 +0200

    Base implementation of the new FX

--- src/main/java/stefan/app/chatapp/ChatApplication.java
+++ src/main/java/stefan/app/chatapp/ChatApplication.java
@@ -1,4 +1,3 @@
-/*
 package stefan.app.chatapp;
 
 import javafx.application.Application;
@@ -7,19 +6,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ChatApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader loader = new FXMLLoader(ChatApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(loader.load(), 400, 500);
+        stage.setTitle("Login");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
-    }
 
+    }
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-}
 
- */
+}
